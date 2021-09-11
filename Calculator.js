13dc1dd527ef09d01e2dd8fc40d04724621a3d8e
Marusha-prog <56190876+Marusha-prog@users.noreply.github.com>
2021-09-11 12:46:49 +0530

    buttons function updated

--- Calculator.js
+++ Calculator.js
@@ -1,1 +1,7 @@
-console.log("Dev bracnch created")
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+document.querySelector('.calc-buttons').addEventListener("click", function(event){
+    
+}
